723b4a98bf786f34b3cb0225c13dc7fe0260df0f
KeebleKapa <keeblekapa@gmail.com>
2024-11-29 21:21:18 -0500

    Initial commit

---
+++ src/main/java/net/keeblekapa/kebbler/api/main/Kebbler.java
@@ -0,0 +1,16 @@
+package net.keeblekapa.kebbler.api.main;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Kebbler implements ModInitializer {
+	public static final String MOD_ID = "kebbler";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/keeblekapa/kebbler/api/main/KebblerDataGenerator.java
@@ -0,0 +1,11 @@
+package net.keeblekapa.kebbler.api.main;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class KebblerDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
